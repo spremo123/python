2937faf3f0184e91891b1493eee3a5eb486ac81e
spremo123 <alexander.spremo@elev.ga.ntig.se>
2019-09-04 13:22:56 +0200

    strängar

---
+++ lek-strings.py
@@ -0,0 +1,40 @@
+
+#strings
+firstName = "alexander"
+middleName = "jovan"
+lastName = "spremo"
+domain = "elev.ga.ntig.se"
+
+nickName = "snej"
+
+print(lastName.capitalize())
+print(firstName.upper())
+
+gamerTag = "JeSuSKUsH"
+
+print(gamerTag.lower())
+
+# alexander.spremo@elev.ga.ntig.se
+
+print(firstName.lower() + "." + lastName.lower() + "@" + domain.lower())
+
+
+
+
+
+
+
+
+
+
+
+tecken = """() parenteser, 
+[] hakparanteser 
+{} måsvingar 
+: kolon
+; semicolon
+, komma
+\" dubbelfnutt
+\' enkelfnutt """
+
+print(tecken[13:37])
